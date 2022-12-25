219c17064ffcd2f37e776b88010e73c2f44006a4
MaAnka324 <115251389+MaAnka324@users.noreply.github.com>
2022-12-25 15:02:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+// function bmi(weight, height) {
+//    let b = weight/Math.pow(height, 2);
+//    if(b <= 18.5){
+//     return "Недостаточный вес"
+//    }
+//    else if(b <= 25){
+//     return "Нормальный"
+//    }
+//    else if(b <= 30){
+//     return "Избыточный вес"
+//    }
+//    else{
+//     return "Ожирение"
+//    }
+//   }
+//   bmi(80, 1.8);
+
+
+// Sum Numbers
+function sum (numbers) {
+    "use strict";
+  let sum = 0;
+    for(let i=0; i<numbers.length; i++){  
+      sum += numbers[i];
+      }
+  return sum
+};
