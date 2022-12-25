78a55cb7a06c529c69a8a167816928a93e7dae78
MaAnka324 <115251389+MaAnka324@users.noreply.github.com>
2022-12-25 15:04:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-
-// function bmi(weight, height) {
-//    let b = weight/Math.pow(height, 2);
-//    if(b <= 18.5){
-//     return "Недостаточный вес"
-//    }
-//    else if(b <= 25){
-//     return "Нормальный"
-//    }
-//    else if(b <= 30){
-//     return "Избыточный вес"
-//    }
-//    else{
-//     return "Ожирение"
-//    }
-//   }
-//   bmi(80, 1.8);
+//1
+function bmi(weight, height) {
+   let b = weight/Math.pow(height, 2);
+   if(b <= 18.5){
+    return "Недостаточный вес"
+   }
+   else if(b <= 25){
+    return "Нормальный"
+   }
+   else if(b <= 30){
+    return "Избыточный вес"
+   }
+   else{
+    return "Ожирение"
+   }
+  }
+  bmi(80, 1.8);
 
 
 // Sum Numbers
